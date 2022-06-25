6bbeefc9c5da620e74865af9d8261dbec46dfe62
Jannikio <theofficialjannik@gmail.com>
2022-06-25 12:37:13 -0400

    started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import Web3Modal from "web3modal";
+import Web3 from "web3";
+import { useRecoilState } from "recoil";
 
-function App() {
+const App = () => {
+  const [account, setAccount] = useRecoilState(accountState);
+
+  const connect = async () => {
+    try {
+      const providerOptions = {
+        walletconnect: {
+          package: WalletConnectProvider,
+          options: {
+            infuraId: "95f65ab099894076814e8526f52c9149", 
+          },
+        },
+      };
+      const web3modal = new Web3Modal({
+        //network: "mainnet",
+        providerOptions,
+      });
+      const provider = await Web3Modal.connect();
+      const web3 = new Web3(provider);
+      const [account] = await web3.eth.getAccounts();
+      setAccount(account);
+    }catch(err)
+     {
+      console.error(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={connect}>Connect</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
